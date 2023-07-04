9bec8325e4aa31100bb3aef67aa43d1f915aa276
PracticeEveryday <developerforus@gmail.com>
2023-07-04 16:22:35 +0900

    fix: readlilne 정리

--- src/libs/readline/readline.service.ts
+++ src/libs/readline/readline.service.ts
@@ -28,49 +28,6 @@ export class ReadlineService {
   }
 
   /**
-   * 서버를 시작하기 위한 사전 질문 프로세스
-   * @param prequalificationsArr 사전 질문지 배열
-   */
-  async processingAboutPrequalifications(prequalificationsArr: { id: number; question: string; answer: string }[]) {
-    const rl = this.getReadline();
-    for (let i = 0; i < prequalificationsArr.length; i++) {
-      const answer = await this.askPrequalification(rl, prequalificationsArr[i].question);
-
-      if (answer !== prequalificationsArr[i].answer) {
-        console.log('😠 답변이 틀립니다 다시 시작해주세요.');
-        rl.close();
-        return false;
-      } else {
-        if (i === prequalificationsArr.length - 1) {
-          console.log('🥳 정답을 모두 맞췄습니다! 5초 후 서버가 시작됩니다!');
-          this.sleep(5000);
-          rl.close();
-          return true;
-        }
-        console.log(`🤔 계속해서 문제를 풀어주세요 ${prequalificationsArr.length - (i + 1)}문제 남았습니다!!\n`);
-      }
-    }
-  }
-
-  // private
-
-  /**
-   * 사전 질문에 대해 물어보기
-   * @param rl 터미널 readline
-   * @param question 질문
-   */
-  private askPrequalification(rl: readline.Interface, question: string): Promise<string> {
-    return new Promise((resolve) => {
-      rl.question(
-        `💬 사전에 등록한 질문을 모두 정확하게 맞추어야 서버가 시작됩니다!!\n🤯 해당 질문에 대해 답변해주세요: ${question}\n`,
-        (answer) => {
-          resolve(answer);
-        },
-      );
-    });
-  }
-
-  /**
    * 등록할 문제에 대해 물어보기
    * @param questionAnswerPairs 질문과 답변 배열
    */
@@ -117,8 +74,8 @@ export class ReadlineService {
   };
 
   /**
-   *
-   * @param questionAnswerPairs
+   * 질문, 답변을 저장하는 프로세스
+   * @param questionAnswerPairs 질문 답변 배열
    */
   private processSaveQuestionAnswerPairs = (questionAnswerPairs: { question: string; answer: string }[]): void => {
     console.log('🚶 질문과 답변들: ');
@@ -149,6 +106,51 @@ export class ReadlineService {
       `UPDATE password.server_infos SET server_status = '${ServerStatusEnum.PENDING}', updatedAt = CURRENT_TIMESTAMP WHERE id = 1`,
     );
   };
+
+  // ------------------------------------------------------------------------------------------------------------
+  //-- 서버 재시작 시 질문을 할 때 readline 프로세스
+  /**
+   * 사전 질문에 대해 물어보기
+   * @param rl 터미널 readline
+   * @param question 질문
+   */
+  private askPrequalification(rl: readline.Interface, question: string): Promise<string> {
+    return new Promise((resolve) => {
+      rl.question(
+        `💬 사전에 등록한 질문을 모두 정확하게 맞추어야 서버가 시작됩니다!!\n🤯 해당 질문에 대해 답변해주세요: ${question}\n`,
+        (answer) => {
+          resolve(answer);
+        },
+      );
+    });
+  }
+
+  /**
+   * 서버를 시작하기 위한 사전 질문 프로세스
+   * @param prequalificationsArr 사전 질문지 배열
+   */
+  async processingAboutPrequalifications(prequalificationsArr: { id: number; question: string; answer: string }[]) {
+    const rl = this.getReadline();
+    for (let i = 0; i < prequalificationsArr.length; i++) {
+      const answer = await this.askPrequalification(rl, prequalificationsArr[i].question);
+
+      if (answer !== prequalificationsArr[i].answer) {
+        console.log('😠 답변이 틀립니다 다시 시작해주세요.');
+        rl.close();
+        return false;
+      } else {
+        if (i === prequalificationsArr.length - 1) {
+          console.log('🥳 정답을 모두 맞췄습니다! 5초 후 서버가 시작됩니다!');
+          this.sleep(5000);
+          rl.close();
+          return true;
+        }
+        console.log(`🤔 계속해서 문제를 풀어주세요 ${prequalificationsArr.length - (i + 1)}문제 남았습니다!!\n`);
+      }
+    }
+  }
+
+  // private
 
   private sleep(ms: number): void {
     const wakeUpTime = Date.now() + ms;
