e7c7cb7902ae00b4520464c3396cdcdaa969e3f1
PracticeEveryday <developerforus@gmail.com>
2023-07-11 09:47:15 +0900

    feat: 설명 주석 추가 interceptor, decorator

--- src/apps/server/common/decorator/connectionPool.decorator.ts
+++ src/apps/server/common/decorator/connectionPool.decorator.ts
@@ -1,5 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+/**
+ * interceptor로 만든 connectionPool을 가져옵니다.
+ */
 export const TransactionManager = createParamDecorator((_data: unknown, ctx: ExecutionContext) => {
   const req = ctx.switchToHttp().getRequest();
   return req.connectionPool;
--- src/apps/server/common/interceptor/transaction.interceptor.ts
+++ src/apps/server/common/interceptor/transaction.interceptor.ts
@@ -5,6 +5,10 @@ import { MysqlService } from '../../../../libs/mysql/mysql.service';
 import { BaseException } from '../customExceptions/exception/base.exception';
 import { CustomUnknownException } from '../customExceptions/exception/unknown.exception';
 
+/**
+ * 트랜젝션을 위한 인터셉터입니다.
+ * 기본적으로 성공 실패시 중복되는 코드를 해당 인터셉터를 재사용함으로서 해결합니다.
+ */
 @Injectable()
 export class TransactionInterceptor implements NestInterceptor {
   constructor(private readonly mysqlService: MysqlService) {}
