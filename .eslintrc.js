cd9943e4b79fddb30482292bfc0e46d9f6b740bb
PracticeEveryday <developerforus@gmail.com>
2023-08-01 11:28:00 +0900

    feat: eslint(함수) 컨벤션 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,10 +14,17 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    // interface에 prefix를 두지 않도록 합니다.
     '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
+    // Typescript가 유추할 수 있도록 하기에 Off합니다.
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'no-new-func': 'error',
+    'no-inner-declarations': 'error',
+    'prefer-arrow-callback': 'error',
+    'implicit-arrow-linebreak': ['error', 'beside'],
+    'object-shorthand': 'error',
+    'lines-between-class-members': ['error', 'always'],
+    'prefer-template': 'error',
     'import/order': [
       'error',
       {
